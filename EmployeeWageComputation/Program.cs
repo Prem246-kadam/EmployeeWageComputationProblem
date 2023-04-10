cb667e5c3621dc19009f94fbd6540d5ed385a8b1
Prem kadam <premkadam246@gmail.com>
2023-04-10 16:21:16 +0530

    Checking Employee Attendence

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace EmployeeWageComputation
+﻿using System;
+
+namespace EmployeeWageComputation
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcom to calculate wage");
+            int isPresent = 1;
+            //welcome message
+            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            //UC1-EmployeeAttendance
+            Random random = new Random();
+            int employeeAttendance = random.Next(0, 2); //0 or 1
+            if (employeeAttendance == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.ReadLine();
         }
     }
 }
